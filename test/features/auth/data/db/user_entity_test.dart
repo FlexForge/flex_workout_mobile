8e60d740f3dee3735bc9007a2984d4fbabb76a7e
Matt Dunn <mattdunn0816@gmail.com>
2024-08-12 18:28:46 -0600

    test(user_entity): added tests

--- test/features/auth/data/db/user_entity_test.dart
+++ test/features/auth/data/db/user_entity_test.dart
@@ -1,9 +1,30 @@
 import 'package:faker/faker.dart';
 import 'package:flex_workout_mobile/features/auth/data/db/user_entity.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('UserEntity', () {
+    group('enums', () {
+      const theme = ThemeMode.light;
+
+      final entity = User(
+        faker.person.firstName(),
+        faker.person.lastName(),
+        faker.internet.email(),
+        preferredTheme: theme,
+      );
+
+      test('get dbPreferredTheme', () {
+        expect(entity.dbPreferredTheme, 1);
+      });
+
+      test('set dbPreferredTheme', () {
+        entity.dbPreferredTheme = ThemeMode.dark.index;
+        expect(entity.preferredTheme, ThemeMode.dark);
+      });
+    });
+
     group('extensions', () {
       test('ConvertUser', () {
         final entity = User(
