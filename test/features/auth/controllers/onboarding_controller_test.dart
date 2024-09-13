653f8e10f73d94fa1aacca2178a7aa6899415d05
Matt Dunn <mattdunn0816@gmail.com>
2024-09-13 14:57:35 -0600

    test: added onboarding controller test

--- test/features/auth/controllers/onboarding_controller_test.dart
+++ test/features/auth/controllers/onboarding_controller_test.dart
@@ -1,13 +1,79 @@
+import 'package:flex_workout_mobile/features/auth/controllers/onboarding_controller.dart';
+import 'package:flex_workout_mobile/features/auth/providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../../test_old/utils.dart';
+import '../../../mocks.dart';
 
 void main() {
-  group(
-    skip: 'Group is unimplemented',
-    'OnboardingController',
-    () {
-      group('build', () {});
+  late MockOnboardingRepository mockOnboardingRepository;
 
-      group('setIsFirstLoad', () {});
-    },
-  );
+  setUp(() {
+    mockOnboardingRepository = MockOnboardingRepository();
+  });
+
+  ProviderContainer createOnboardingContainer() {
+    return createContainer(
+      overrides: [
+        onboardingRepositoryProvider
+            .overrideWithValue(mockOnboardingRepository),
+      ],
+    );
+  }
+
+  group('OnboardingController', () {
+    group('build', () {
+      test('should return true if first load', () {
+        when(() => mockOnboardingRepository.getIsFirstLoad())
+            .thenReturn(right(true));
+
+        final container = createOnboardingContainer();
+        final res = container.read(onboardingControllerProvider);
+
+        final loadListener = isFirstLoadListener.value;
+
+        expect(res, true);
+        expect(loadListener, true);
+      });
+    });
+
+    group('setIsFirstLoad', () {
+      test('should set isFirstLoad to true', () async {
+        when(
+          () => mockOnboardingRepository.setIsFirstLoad(isFirstLoad: true),
+        ).thenAnswer((_) => Future.value(right(true)));
+
+        final ref = createOnboardingContainer();
+        await ref
+            .read(onboardingControllerProvider.notifier)
+            .setIsFirstLoad(isFirstLoad: true);
+
+        final loadListener = isFirstLoadListener.value;
+        final res = ref.read(onboardingControllerProvider);
+
+        expect(res, true);
+        expect(loadListener, true);
+      });
+
+      test('should set isFirstLoad to false', () async {
+        when(
+          () => mockOnboardingRepository.setIsFirstLoad(isFirstLoad: false),
+        ).thenAnswer((_) => Future.value(right(false)));
+
+        final ref = createOnboardingContainer();
+        await ref
+            .read(onboardingControllerProvider.notifier)
+            .setIsFirstLoad(isFirstLoad: false);
+
+        final loadListener = isFirstLoadListener.value;
+        final res = ref.read(onboardingControllerProvider);
+
+        expect(res, false);
+        expect(loadListener, false);
+      });
+    });
+  });
 }
---
+++ test/mocks.dart
@@ -0,0 +1,4 @@
+import 'package:flex_workout_mobile/features/auth/data/repositories/onboarding_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockOnboardingRepository extends Mock implements OnboardingRepository {}
