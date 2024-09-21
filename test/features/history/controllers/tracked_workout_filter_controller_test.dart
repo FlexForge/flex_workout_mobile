e3d5ac6aa0680aea16bcfc0e39ca2dca81c73c3a
Matt Dunn <mattdunn0816@gmail.com>
2024-09-21 01:00:09 -0600

    fix: flutter analyze

--- test/features/history/controllers/tracked_workout_filter_controller_test.dart
+++ test/features/history/controllers/tracked_workout_filter_controller_test.dart
@@ -1,13 +1,6 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../../../utils.dart';
-
 void main() {
-  ProviderContainer createFilterContainer() {
-    return createContainer();
-  }
-
   group('TrackedWorkoutFilterController', () {
     group('build', () {});
 
