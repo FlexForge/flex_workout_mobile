daaec87e14908decf16d6725f6670940fcd1ee84
Matt Dunn <mattdunn0816@gmail.com>
2024-10-06 22:09:55 -0600

    feat: added tabs to filter modal

--- lib/features/tracker/ui/components/exercise_selection_filters.dart
+++ lib/features/tracker/ui/components/exercise_selection_filters.dart
@@ -19,9 +19,8 @@ class ExerciseSelectionFiltersModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScrollableSheet(
+    return DraggableSheet(
       initialExtent: const Extent.proportional(0.5),
-      minExtent: const Extent.proportional(0.5),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(16),
         child: child,
@@ -30,160 +29,184 @@ class ExerciseSelectionFiltersModal extends StatelessWidget {
   }
 }
 
-class ExerciseSelectionFilters extends ConsumerWidget {
+class ExerciseSelectionFilters extends StatefulWidget {
   const ExerciseSelectionFilters({super.key});
 
   static const routePath = 'exercise_selection_filters';
   static const routeName = 'exercise_selection_filters';
 
   @override
+  State<ExerciseSelectionFilters> createState() =>
+      _ExerciseSelectionFiltersState();
+}
+
+class _ExerciseSelectionFiltersState extends State<ExerciseSelectionFilters>
+    with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: 3,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SheetContentScaffold(
+      backgroundColor: context.colors.backgroundPrimary,
+      body: Column(
+        children: [
+          const SizedBox(height: AppLayout.p4),
+          Row(
+            children: [
+              const SizedBox(width: AppLayout.p4),
+              Text(
+                'Filters',
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                style: context.typography.headlineMedium
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+              const Spacer(),
+              FlexButton(
+                onPressed: context.pop,
+                icon: Symbols.close,
+                iconSize: 16,
+                iconWeight: 700,
+                padding: const EdgeInsets.all(AppLayout.p1),
+                borderRadius: AppLayout.roundedRadius,
+                iconColor: context.colors.foregroundSecondary,
+                backgroundColor: context.colors.backgroundTertiary,
+              ),
+              const SizedBox(width: AppLayout.p2),
+            ],
+          ),
+          // const SizedBox(height: AppLayout.p2),
+          ColoredBox(
+            color: context.colors.backgroundPrimary,
+            child: TabBar(
+              tabs: [
+                Tab(
+                  child: Row(
+                    children: [
+                      const Icon(Symbols.workspaces, size: 20),
+                      const SizedBox(width: AppLayout.p1),
+                      Text(
+                        'Muscle Groups',
+                        style: context.typography.labelMedium,
+                      ),
+                    ],
+                  ),
+                ),
+                Tab(
+                  child: Row(
+                    children: [
+                      const Icon(Symbols.exercise, size: 20),
+                      const SizedBox(width: AppLayout.p1),
+                      Text(
+                        'Equipment',
+                        style: context.typography.labelMedium,
+                      ),
+                    ],
+                  ),
+                ),
+                Tab(
+                  child: Row(
+                    children: [
+                      const Icon(Symbols.directions_run, size: 20, weight: 700),
+                      const SizedBox(width: AppLayout.p1),
+                      Text(
+                        'Movement Patterns',
+                        style: context.typography.labelMedium,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+              controller: _tabController,
+              isScrollable: true,
+              indicatorSize: TabBarIndicatorSize.tab,
+              labelColor: context.colors.foregroundPrimary,
+              indicatorColor: context.colors.foregroundPrimary,
+              unselectedLabelColor: context.colors.foregroundSecondary,
+              dividerColor: context.colors.divider,
+              indicatorWeight: 3,
+              overlayColor: WidgetStateProperty.all(
+                context.colors.backgroundTertiary,
+              ),
+              dividerHeight: 1,
+              tabAlignment: TabAlignment.center,
+            ),
+          ),
+          const SizedBox(height: AppLayout.p4),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: const [
+                MuscleGroupsFilter(),
+                MuscleGroupsFilter(),
+                MuscleGroupsFilter(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MuscleGroupsFilter extends ConsumerWidget {
+  const MuscleGroupsFilter({super.key});
+
+  @override
   Widget build(BuildContext context, WidgetRef ref) {
     final muscleGroups = ref.watch(muscleGroupListControllerProvider);
 
-    return SheetContentScaffold(
-      backgroundColor: context.colors.backgroundPrimary,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: AppLayout.p4),
-        child: ListView(
-          children: [
-            const SizedBox(height: AppLayout.p2),
-            Row(
-              children: [
-                Text(
-                  'Filters',
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  style: context.typography.headlineMedium
-                      .copyWith(fontWeight: FontWeight.bold),
-                ),
-                const Spacer(),
-                FlexButton(
-                  onPressed: context.pop,
-                  icon: Symbols.close,
-                  iconSize: 16,
-                  iconWeight: 700,
-                  padding: const EdgeInsets.all(AppLayout.p1),
-                  borderRadius: AppLayout.roundedRadius,
-                  iconColor: context.colors.foregroundSecondary,
-                  backgroundColor: context.colors.backgroundSecondary,
-                ),
-              ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: AppLayout.p2),
+      child: LayoutGrid(
+        gridFit: GridFit.loose,
+        columnGap: AppLayout.p2,
+        rowGap: AppLayout.p2,
+        columnSizes: [1.fr, 1.fr],
+        rowSizes: List.generate(10, (_) => auto),
+        children: [
+          RadioListItem(
+            onPressed: () {
+              ref.read(muscleGroupFilterControllerProvider.notifier).clear();
+            },
+            selected: ref.watch(muscleGroupFilterControllerProvider).isEmpty,
+            padding: const EdgeInsets.only(right: AppLayout.p1),
+            name: 'All',
+            icon: Symbols.apps,
+            value: null,
+          ),
+          ...muscleGroups.map(
+            (group) => RadioListItem(
+              onPressed: () {
+                ref
+                    .read(muscleGroupFilterControllerProvider.notifier)
+                    .handle(group);
+              },
+              selected: ref
+                  .watch(muscleGroupFilterControllerProvider)
+                  .contains(group),
+              padding: const EdgeInsets.only(right: AppLayout.p1),
+              name: group.name,
+              icon: Symbols.apps,
+              value: null,
             ),
-            const SizedBox(height: AppLayout.p4),
-            Text(
-              'Muscle Groups',
-              overflow: TextOverflow.ellipsis,
-              maxLines: 2,
-              style: context.typography.headlineMedium
-                  .copyWith(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: AppLayout.p2),
-            LayoutGrid(
-              columnGap: AppLayout.p2,
-              rowGap: AppLayout.p2,
-              columnSizes: [1.fr, 1.fr],
-              rowSizes: List.generate(10, (_) => auto),
-              children: [
-                RadioListItem(
-                  onPressed: () {
-                    ref
-                        .read(muscleGroupFilterControllerProvider.notifier)
-                        .clear();
-                  },
-                  selected:
-                      ref.watch(muscleGroupFilterControllerProvider).isEmpty,
-                  padding: const EdgeInsets.only(right: AppLayout.p1),
-                  name: 'All',
-                  icon: Symbols.apps,
-                  value: null,
-                ),
-                ...muscleGroups.map(
-                  (group) => RadioListItem(
-                    onPressed: () {
-                      ref
-                          .read(muscleGroupFilterControllerProvider.notifier)
-                          .handle(group);
-                    },
-                    selected: ref
-                        .watch(muscleGroupFilterControllerProvider)
-                        .contains(group),
-                    padding: const EdgeInsets.only(right: AppLayout.p1),
-                    name: group.name,
-                    icon: Symbols.apps,
-                    value: null,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: AppLayout.p4),
-            Text(
-              'Movement Patterns',
-              overflow: TextOverflow.ellipsis,
-              maxLines: 2,
-              style: context.typography.headlineMedium
-                  .copyWith(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: AppLayout.p2),
-            Wrap(
-              spacing: AppLayout.p2,
-              runSpacing: AppLayout.p2,
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                const RadioListItem(
-                  shrinkWrap: true,
-                  padding: EdgeInsets.only(right: AppLayout.p1),
-                  name: 'All',
-                  icon: Symbols.apps,
-                  value: null,
-                ),
-                ...MovementPattern.values.map(
-                  (pattern) => RadioListItem(
-                    onPressed: () {},
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.only(right: AppLayout.p1),
-                    name: pattern.readableName,
-                    icon: Symbols.apps,
-                    value: pattern,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: AppLayout.p4),
-            Text(
-              'Equipment',
-              overflow: TextOverflow.ellipsis,
-              maxLines: 2,
-              style: context.typography.headlineMedium
-                  .copyWith(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: AppLayout.p2),
-            Wrap(
-              spacing: AppLayout.p2,
-              runSpacing: AppLayout.p2,
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                const RadioListItem(
-                  shrinkWrap: true,
-                  padding: EdgeInsets.only(right: AppLayout.p1),
-                  name: 'All',
-                  icon: Symbols.apps,
-                  value: null,
-                ),
-                ...Equipment.values.map(
-                  (equipment) => RadioListItem(
-                    onPressed: () {},
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.only(right: AppLayout.p1),
-                    name: equipment.readableName,
-                    icon: Symbols.apps,
-                    value: equipment,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: AppLayout.bottomBuffer),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
