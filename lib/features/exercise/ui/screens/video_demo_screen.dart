c14cb0388ab814f6579cb2aa4766dc3d905de2c6
Matt Dunn <mattdunn0816@gmail.com>
2024-10-09 16:24:51 -0600

    style: added styling to the video player

--- lib/features/exercise/ui/screens/video_demo_screen.dart
+++ lib/features/exercise/ui/screens/video_demo_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flex_workout_mobile/core/common/ui/components/back_button.dart';
+import 'package:flex_workout_mobile/core/common/ui/components/button.dart';
 import 'package:flex_workout_mobile/core/common/ui/components/info_card.dart';
 import 'package:flex_workout_mobile/core/extensions/ui_extensions.dart';
 import 'package:flex_workout_mobile/core/theme/app_layout.dart';
@@ -19,6 +20,7 @@ class DemoVideoDisplay extends StatefulWidget {
 
 class _DemoVideoDisplayState extends State<DemoVideoDisplay> {
   late YoutubePlayerController _controller;
+  var duration = 0;
 
   @override
   void initState() {
@@ -49,9 +51,19 @@ class _DemoVideoDisplayState extends State<DemoVideoDisplay> {
   Widget build(BuildContext context) {
     return YoutubePlayerBuilder(
       player: YoutubePlayer(
-        showVideoProgressIndicator: true,
-        progressIndicatorColor: Colors.blueAccent,
+        actionsPadding: const EdgeInsets.symmetric(
+          horizontal: AppLayout.p4,
+          vertical: AppLayout.p2,
+        ),
+        bottomActions: const [
+          Expanded(
+            child: VideoDisplayControls(),
+          ),
+        ],
         controller: _controller,
+        onReady: () => setState(() {
+          duration = _controller.metadata.duration.inMinutes;
+        }),
       ),
       builder: (context, player) => Scaffold(
         backgroundColor: context.colors.backgroundSecondary,
@@ -74,12 +86,20 @@ class _DemoVideoDisplayState extends State<DemoVideoDisplay> {
           ),
           child: Column(
             children: [
-              player,
+              Container(
+                decoration: BoxDecoration(
+                  color: context.colors.blue,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(AppLayout.cornerRadius),
+                  ),
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: Container(child: player),
+              ),
               const SizedBox(height: AppLayout.p4),
               const InfoCard(
                 icon: Symbols.info,
-                info:
-                    'This video provides a quick demonstration of the exercise '
+                info: 'This video provides a quick demo of the exercise '
                     'showcasing the technique, and highlighting some queues '
                     'to keep in mind while performing this exercise.',
               ),
@@ -89,4 +109,107 @@ class _DemoVideoDisplayState extends State<DemoVideoDisplay> {
       ),
     );
   }
+}
+
+class VideoDisplayControls extends StatefulWidget {
+  const VideoDisplayControls({this.controller, super.key});
+
+  final YoutubePlayerController? controller;
+
+  @override
+  State<VideoDisplayControls> createState() => _VideoDisplayControlsState();
+}
+
+class _VideoDisplayControlsState extends State<VideoDisplayControls> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final controller = YoutubePlayerController.of(context);
+    if (controller == null) {
+      assert(
+        widget.controller != null,
+        '\n\nNo controller could be found in the provided context.\n\n'
+        'Try passing the controller explicitly.',
+      );
+      _controller = widget.controller!;
+    } else {
+      _controller = controller;
+    }
+    _controller
+      ..removeListener(listener)
+      ..addListener(listener);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(listener);
+    super.dispose();
+  }
+
+  void listener() {
+    if (mounted) setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            const SizedBox(width: AppLayout.p2),
+            RichText(
+              text: TextSpan(
+                text: durationFormatter(_controller.value.position),
+                style: context.typography.labelMedium
+                    .copyWith(fontWeight: FontWeight.w700),
+                children: [
+                  TextSpan(
+                    text: ' / ',
+                    style: context.typography.labelMedium.copyWith(
+                      color: context.colors.foregroundSecondary,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  TextSpan(
+                    text: durationFormatter(_controller.metadata.duration),
+                    style: context.typography.labelMedium.copyWith(
+                      color: context.colors.foregroundSecondary,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Spacer(),
+            FlexButton(
+              padding: const EdgeInsets.all(AppLayout.p2),
+              icon: Symbols.fullscreen,
+              iconWeight: 900,
+              backgroundColor: Colors.transparent,
+              onPressed: () => _controller.toggleFullScreenMode(),
+            ),
+          ],
+        ),
+        ProgressBar(
+          colors: ProgressBarColors(
+            backgroundColor: context.colors.foregroundSecondary,
+            bufferedColor: context.colors.foregroundPrimary,
+            playedColor: context.colors.blue,
+            handleColor: context.colors.blue,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+String durationFormatter(Duration duration) {
+  String twoDigits(int n) => n.toString().padLeft(2, '0');
+  final twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60).abs());
+  final twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60).abs());
+  return '${duration.inHours > 0 ? '${twoDigits(duration.inHours)}:' : ''}'
+      '$twoDigitMinutes:'
+      '$twoDigitSeconds';
 }
