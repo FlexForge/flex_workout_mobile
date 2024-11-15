0a58d613495bf660a410f8345d898984c3929d97
Mitchell Joram <mitchell.a.joram@gmail.com>
2024-11-14 18:29:26 -0700

    feat(workout): ignore flutter todo format

--- lib/features/workout/data/models/workout_model.dart
+++ lib/features/workout/data/models/workout_model.dart
@@ -125,7 +125,8 @@ class SupersetWorkoutSectionModel
       }
     }
 
-    /// TODO(mitchelljoram): sorted and take first and last - maybe delete this later
+    /// ignore: flutter_style_todos
+    /// TODO: sorted and take first and last - maybe delete this later
     final sortedTotalSets = SplayTreeMap<String, int>.from(
       totalSets,
       (k1, k2) => totalSets[k1]!.compareTo(totalSets[k2]!),
@@ -142,7 +143,8 @@ class SupersetWorkoutSectionModel
       sortedTotalSets.entries.last.key: sortedTotalSets.entries.last.value,
     };
 
-    /// TODO(mitchelljoram): all total sets - maybe delete this later
+    /// ignore: flutter_style_todos
+    /// TODO: all total sets - maybe delete this later
     // return totalSets;
   }
 
